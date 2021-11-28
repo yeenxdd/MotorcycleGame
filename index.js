5bf04e0cd0fb830c4afa376ac7790fd4a7a1686f
Ye En <yeenxdd@gmail.com>
2021-11-28 17:57:59 +0800

    create canvas

---
+++ index.js
@@ -0,0 +1,12 @@
+var c = document.createElement("canvas");
+var ctx = c.getContext("2d");
+c.width = 500;
+c.height = 350;
+document.body.appendChild(c);
+
+function loop(){
+    ctx.fillRect(0, 0, c.width, c.height);
+    requestAnimationFrame(loop);
+}
+
+loop();
