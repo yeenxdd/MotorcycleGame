b48d6c790ba2408dc06cef1c5b7a814b08356438
Ye En <yeenxdd@gmail.com>
2021-11-28 18:12:02 +0800

    create moveable, playable terrain

--- index.js
+++ index.js
@@ -10,22 +10,27 @@ while (perm.length < 255){
     perm.push(val);
 }
 
-var lerp = (a,b,t) => a + (b-a) * t;
+var lerp = (a,b,t) => a + (b-a) * (1-Math.cos(t * Math.PI)) / 2;
 var noise = x => {
+    x = x * 0.01 % 255;
     return lerp(perm[Math.floor(x)], perm[Math.ceil(x)], x - Math.floor(x));
 }
 
+var t = 0;
 function loop(){
+    t += 1;
     ctx.fillStyle = "#19f";
     ctx.fillRect(0, 0, c.width, c.height);
 
     ctx.fillStyle = "black";
     ctx.beginPath();
+    ctx.moveTo(0, c.height);
     for (let i = 0; i < c.width; i++) {
-        ctx.lineTo(i, noise(i));
+        ctx.lineTo(i, c.height - noise(t + i) * 0.25);
     }
+    ctx.lineTo(c.width,c.height);
     ctx.fill();
-    
+
     requestAnimationFrame(loop);
 }
 
