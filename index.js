9dc0d0ba3f6e676823a86912cddeb09efe6ad807
Ye En <yeenxdd@gmail.com>
2021-11-28 18:07:25 +0800

    create terrain

--- index.js
+++ index.js
@@ -4,8 +4,28 @@ c.width = 500;
 c.height = 350;
 document.body.appendChild(c);
 
+var perm = [];
+while (perm.length < 255){
+    while(perm.includes(val = Math.floor(Math.random()*255)));
+    perm.push(val);
+}
+
+var lerp = (a,b,t) => a + (b-a) * t;
+var noise = x => {
+    return lerp(perm[Math.floor(x)], perm[Math.ceil(x)], x - Math.floor(x));
+}
+
 function loop(){
+    ctx.fillStyle = "#19f";
     ctx.fillRect(0, 0, c.width, c.height);
+
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+    for (let i = 0; i < c.width; i++) {
+        ctx.lineTo(i, noise(i));
+    }
+    ctx.fill();
+    
     requestAnimationFrame(loop);
 }
 
