f323e2dcc757b051591fcde16769743a40376f55
Ye En <yeenxdd@gmail.com>
2021-11-28 20:54:43 +0800

    motorcycle moves according to terrain

--- index.js
+++ index.js
@@ -26,14 +26,17 @@ var player = new function(){
     this.img.src = "foodpandaRider.png";
     this.draw = function(){
         var p1 = c.height - noise(t + this.x) * 0.25;
-        if(p1 > this.y){
+        if(p1 - 50 > this.y){
             this.ySpeed -= 0.1;
+        }else{
+            this.y = p1 - 50;
+            this.ySpeed = 0;
         }
         this.y -= this.ySpeed;
 
         ctx.save();
         ctx.translate(this.x, this.y);
-        ctx.drawImage(this.img, 0, 0, this.img.width/25, this.img.height/25);
+        ctx.drawImage(this.img, 0, 0, this.img.width/35, this.img.height/35);
         ctx.restore();
     }
 }
